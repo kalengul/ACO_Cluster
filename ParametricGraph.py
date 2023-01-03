8a8b01ff3a9d9ad1f2fc13abef3b5f6da53d7b6f
“Kalengul” <kalengul@mail.ru>
2023-01-03 22:21:12 +0300

    1.4.5(4)

--- ParametricGraph.py
+++ ParametricGraph.py
@@ -228,7 +228,7 @@ def GoAntNextNode(ParametricGraph,ArrayNode):
     sum=0
     i=0
     while i<len(ArrayNode):
-       if (PG.EndAllSolution==0) or (ParametricGraph.AllSolution!=ParametricGraph,ArrayNode[i].KolSolution): 
+       if (PG.EndAllSolution==0) or (ParametricGraph.AllSolution!=ArrayNode[i].KolSolution): 
            sum=sum + ProbabilityNode(ParametricGraph,ArrayNode[i])
        probability.append(sum)
        i=i+1
